f0889c5499e6017802bda2fcc6bfb1624511f843
Sonu sharma <ss096044@gmail.com>
2026-07-31 15:51:58 +0530

    hii

---
+++ FRONTEND/REACT/day20-Authentication_redux/src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {createBrowserRouter, RouterProvider} from 'react-router'
+
+const AppRoutes = () => {
+    let router  = createBrowserRouter([
+        {
+            path:'',
+            element:
+        }
+    ])
+  return <RouterProvider />
+}
+
+export default AppRoutes
+
